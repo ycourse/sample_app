c1b68a13ec32a090988da6917d5d0e9208123c3c
Benjamin Martin <hello@ycourse.fr>
2014-04-24 13:15:03 +0200

    pages ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
 
   def home
+  	@titre = "Accueil"
   end
 
 
   def contact
+  	@titre = "Contact"
+  end
+
+
+  def about
+  	@titre = "A propos"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/contact'
 
-  root 'static#page'
+  root 'pages#home'
 
   get 'static/home'
 
